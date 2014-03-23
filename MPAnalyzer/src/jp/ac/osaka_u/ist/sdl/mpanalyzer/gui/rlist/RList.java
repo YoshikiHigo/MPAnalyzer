3249831d7b875fb0bf29d1a289a6871f1ce886e7
YoshikiHigo <yoshiki.higo@gmail.com>
2014-03-23 19:11:41 +0900

    Directrly getting revisions from a given repository

--- MPAnalyzer/src/jp/ac/osaka_u/ist/sdl/mpanalyzer/gui/rlist/RList.java
+++ MPAnalyzer/src/jp/ac/osaka_u/ist/sdl/mpanalyzer/gui/rlist/RList.java
@@ -2,6 +2,7 @@ package jp.ac.osaka_u.ist.sdl.mpanalyzer.gui.rlist;
 
 import java.awt.Color;
 import java.awt.GridLayout;
+import java.io.File;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
@@ -17,8 +18,17 @@ import javax.swing.JScrollPane;
 import javax.swing.border.LineBorder;
 import javax.swing.border.TitledBorder;
 
+import jp.ac.osaka_u.ist.sdl.mpanalyzer.Config;
+import jp.ac.osaka_u.ist.sdl.mpanalyzer.StringUtility;
 import jp.ac.osaka_u.ist.sdl.mpanalyzer.data.Revision;
 import jp.ac.osaka_u.ist.sdl.mpanalyzer.db.ReadOnlyDAO;
+
+import org.tmatesoft.svn.core.ISVNLogEntryHandler;
+import org.tmatesoft.svn.core.SVNException;
+import org.tmatesoft.svn.core.SVNLogEntry;
+import org.tmatesoft.svn.core.SVNURL;
+import org.tmatesoft.svn.core.internal.io.fs.FSRepositoryFactory;
+import org.tmatesoft.svn.core.io.SVNRepository;
 
 public class RList extends JPanel {
 
@@ -46,11 +56,13 @@ public class RList extends JPanel {
 						}
 					});
 
-//			final int threshold = 100;
-			revisions.addAll(ReadOnlyDAO.getInstance().getRevisions());
-//			this.setLayout(new GridLayout(
-//					revisions.size() < threshold ? revisions.size() : threshold,
-//					1));
+			//revisions.addAll(ReadOnlyDAO.getInstance().getRevisions());
+			revisions.addAll(getRevisionsFromRepository());
+			
+			// final int threshold = 100;
+			// this.setLayout(new GridLayout(
+			// revisions.size() < threshold ? revisions.size() : threshold,
+			// 1));
 			this.setLayout(new GridLayout(revisions.size(), 1));
 			int number = 0;
 			for (final Revision revision : revisions) {
@@ -99,6 +111,57 @@ public class RList extends JPanel {
 		// } else {
 		// return 0;
 		// }
+	}
 
+	private SortedSet<Revision> getRevisionsFromRepository() {
+
+		try {
+
+			final String PATH_TO_REPOSITORY = Config.getPATH_TO_REPOSITORY();
+			final String LANGUAGE = Config.getLanguage();
+
+			final SVNURL url = SVNURL.fromFile(new File(PATH_TO_REPOSITORY));
+			FSRepositoryFactory.setup();
+			final SVNRepository repository = FSRepositoryFactory.create(url);
+
+			final long startRevision = Config.getStartRevision();
+			final long endRevision = Config.getEndRevision();
+
+			final SortedSet<Revision> revisions = new TreeSet<Revision>();
+
+			repository.log(null, startRevision, endRevision, true, true,
+					new ISVNLogEntryHandler() {
+						public void handleLogEntry(SVNLogEntry logEntry)
+								throws SVNException {
+							for (final Object key : logEntry.getChangedPaths()
+									.keySet()) {
+								final String path = (String) key;
+								final int number = (int) logEntry.getRevision();
+								final String date = StringUtility
+										.getDateString(logEntry.getDate());
+								final String message = logEntry.getMessage();
+								final Revision revision = new Revision(number,
+										date, message);
+								if (LANGUAGE.equalsIgnoreCase("JAVA")
+										&& StringUtility.isJavaFile(path)) {
+									revisions.add(revision);
+									break;
+								} else if (LANGUAGE.equalsIgnoreCase("C")
+										&& StringUtility.isCFile(path)) {
+									revisions.add(revision);
+									break;
+								}
+							}
+						}
+					});
+
+			return revisions;
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.exit(0);
+		}
+
+		return new TreeSet<Revision>();
 	}
 }
